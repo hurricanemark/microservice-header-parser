a54b437583cf0d99af5f3ec64a1f791a0bd3571d
markn <markn@aesclever.com>
2022-02-18 18:15:53 -0800

    Implement app.get("/api/whoami", (req,res)=>{})

--- server.js
+++ server.js
@@ -21,8 +21,29 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+/*
+A request to /api/whoami should return a JSON object with your IP address in the ipaddress key.
+
+A request to /api/whoami should return a JSON object with your preferred language in the language key.
+
+A request to /api/whoami should return a JSON object with your software in the software key.
+
+Expected output:
+{"ipaddress":"::ffff:159.20.14.100","language":"en-US,en;q=0.5",
+"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+*/
+app.get("/api/whoami", function (req, res) {
+  const header = req.headers
+  console.log(header);
+  console.log()
+  console.log("ipaddress:", header.host)
+  console.log("language:", header["accept-language"]);
+  console.log("software:", header["user-agent"]);
+  res.json({
+    ipaddress: header.host, 
+    language: header["accept-language"],
+    software: header["user-agent"]});
 });
 
 
